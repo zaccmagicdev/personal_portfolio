5e598cc62286a9f8dc82cadec92a70ec67280089
Zachary Wedel <zwede710@gmail.com>
2024-12-03 18:14:27 -0600

    WIP: Working on paralax wallpaper

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-    
+      <div className='stars' />
+      <div className='stars2' />
+      <div className='stars3' />
     </div>
   );
 }
