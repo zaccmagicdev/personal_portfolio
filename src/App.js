2dce8307abdc9de36db1dc4a31ea3f60b361e036
Zachary Wedel <zwede710@gmail.com>
2024-12-14 14:26:32 -0600

    UPDATE: Back working again from mental health break, have a background I'm happy wiht

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <div className='stars' />
+      <div className='stars1' />
       <div className='stars2' />
       <div className='stars3' />
     </div>
