845f3778a69c96f1fe2ddbb33261fd9a74aa02c6
Zachary Wedel <zwede710@gmail.com>
2025-01-02 17:25:37 -0600

    UPDATE: Added font and made it work

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,9 +3,14 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
+
       <div className='stars1' />
       <div className='stars2' />
       <div className='stars3' />
+
+      <section className='app__section'>
+        <h1>My name is Zachary Wedel and I am a...</h1>
+      </section>
     </div>
   );
 }
