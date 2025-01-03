3ce48e0682fb2350393d88a996f2c4169e24698d
Zachary Wedel <zwede710@gmail.com>
2025-01-03 12:05:18 -0600

    WIP: Working on floating design and smooth carousel scrolling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,12 +6,17 @@ function App() {
       <div className="stars stars1" />
       <div className="stars stars2" />
       <div className="stars stars3" />
-      
+
       <div className="App">
-        <section className="app__section">
+        <section className="app__section app__section-title">
           <h1 className="app__main-text">
             My name is Zachary Wedel and I am a...
           </h1>
+        </section>
+        <section className="app__section">
+          <h2>What am I capable of?</h2>
+          <p>I am a self-driven, yet compassionate and willing full stack software developer with 2 years in this wonderful industry.
+          </p>
         </section>
       </div>
     </>
