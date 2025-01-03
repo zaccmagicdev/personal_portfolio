78d442d97e622a9781423ab58ee814c23f573638
Zachary Wedel <zwede710@gmail.com>
2025-01-03 11:33:26 -0600

    FIX: Patched annoying star rendering bug that messed up page height

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,20 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-
-      <div className='stars stars1' />
-      <div className='stars stars2' />
-      <div className='stars stars3' />
-
-      <section className='app__section'>
-        <h1 className='app__main-text'>My name is Zachary Wedel and I am a...</h1>
-      </section>
-    </div>
+    <>
+      <div className="stars stars1" />
+      <div className="stars stars2" />
+      <div className="stars stars3" />
+      
+      <div className="App">
+        <section className="app__section">
+          <h1 className="app__main-text">
+            My name is Zachary Wedel and I am a...
+          </h1>
+        </section>
+      </div>
+    </>
   );
 }
 
