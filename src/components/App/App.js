9812c7b282a4475bdda7bc82d1df5e76b0f4e1cd
Zachary Wedel <zwede710@gmail.com>
2025-01-06 11:23:54 -0600

    UPDATE: Skeleton for layout just about complete for scrolling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import "./App.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Parallax, Pagination } from 'swiper/modules';
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 function App() {
-  const swiper = {
-    pagesPerSlide: 1,
-    mousewheel: true,
-  };
-
   return (
     <>
       <div className="stars stars1" />
@@ -13,20 +14,95 @@ function App() {
       <div className="stars stars3" />
 
       <div className="App">
-        <div class="swiper">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide">Slide 1</div>
-            <div class="swiper-slide">Slide 2</div>
-            <div class="swiper-slide">Slide 3</div>
+      <>
+      <Swiper
+        style={{
+          '--swiper-navigation-color': '#FF69B4',
+          '--swiper-pagination-color': '#FF69B4',
+          '--swiper-pagination-bullet-inactive-color': '#5DF15D',
+          '--swiper-pagination-bullet-inactive-opacity': '1',
+          "--swiper-pagination-bullet-size": "12px",
+        }}
+        speed={1200}
+        direction="vertical"
+        parallax={true}
+        pagination={{
+          clickable: true,
+        }}
+        mousewheel={true}
+        navigation={true}
+        modules={[Parallax, Pagination ]}
+        className="appParallaxSlider"
+        slidesPerView={1}
+      >
+        <div
+          slot="container-start"
+          className="parallax-bg"
+          style={{
+            'background-image':
+              'url(https://swiperjs.com/demos/images/nature-1.jpg)',
+          }}
+          data-swiper-parallax="-23%"
+        ></div>
+        <SwiperSlide>
+          <div className="title" data-swiper-parallax="-300">
+            Slide 1
           </div>
-
-          <div class="swiper-pagination"></div>
-
-          <div class="swiper-button-prev"></div>
-          <div class="swiper-button-next"></div>
-
-          <div class="swiper-scrollbar"></div>
-        </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtitle
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="title" data-swiper-parallax="-300">
+            Slide 2
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtitle
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="title" data-swiper-parallax="-300">
+            Slide 3
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtitle
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
         {/*<section className="app__section app__section-title">
           <h1 className="app__main-text">
             My name is Zachary Wedel and I am a...
