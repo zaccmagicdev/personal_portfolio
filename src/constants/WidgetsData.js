0bf3d3ce430168bce380a45a38a07086c90c3386
Zachary Wedel <zwede710@gmail.com>
2025-02-20 13:05:05 -0600

    UPDATE: content fits better in widget box

--- src/constants/WidgetsData.js
+++ src/constants/WidgetsData.js
@@ -5,5 +5,33 @@ export const widgetData = [
         githublink: 'https://github.com/zaccmagicdev/se_code_cracker_frontend',
         gifURL: 'https://media.tenor.com/JVn1Ve5xIbwAAAAM/ready-set.gif',
         description: 'blah blah blah blah'
+    },
+    {
+        id: 1,
+        name: 'CodeCracker',
+        githublink: 'https://github.com/zaccmagicdev/se_code_cracker_frontend',
+        gifURL: 'https://media.tenor.com/JVn1Ve5xIbwAAAAM/ready-set.gif',
+        description: 'blah blah blah blah'
+    },
+    {
+        id: 2,
+        name: 'CodeCracker',
+        githublink: 'https://github.com/zaccmagicdev/se_code_cracker_frontend',
+        gifURL: 'https://media.tenor.com/JVn1Ve5xIbwAAAAM/ready-set.gif',
+        description: 'blah blah blah blah'
+    },
+    {
+        id: 3,
+        name: 'CodeCracker',
+        githublink: 'https://github.com/zaccmagicdev/se_code_cracker_frontend',
+        gifURL: 'https://media.tenor.com/JVn1Ve5xIbwAAAAM/ready-set.gif',
+        description: 'blah blah blah blah'
+    },
+    {
+        id: 4,
+        name: 'CodeCracker',
+        githublink: 'https://github.com/zaccmagicdev/se_code_cracker_frontend',
+        gifURL: 'https://media.tenor.com/JVn1Ve5xIbwAAAAM/ready-set.gif',
+        description: 'blah blah blah blah'
     }
 ]
