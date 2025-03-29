a5548b909e3ff347e44d07e5d107440e15cbafbf
Zachary Wedel <zwede710@gmail.com>
2025-03-29 12:28:58 -0500

    UPDATE: Adding static animation confirmed

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -4,18 +4,21 @@ import { authorImages } from "../../constants/SlideShowImages";
 import generateRandom from "../../utils/utils";
 
 function SlideShow() {
+  const imgRef = React.useRef('');
   const [index, setIndex] = React.useState(0);
   const arraySize = authorImages.length - 1;
+
+  const addStatic = () => {}
 
   const nextImg = () => {index >= arraySize ? setIndex(0) : setIndex(index + 1)}
   const previousImg = () => {index <= 0 ? setIndex(arraySize) : setIndex(index - 1)}
   
-  React.useEffect(() => {setIndex(generateRandom(arraySize))}, [])
+  React.useEffect(() => {setIndex(generateRandom(arraySize)); console.log(imgRef)}, [])
 
   return (
   <div className="slideshow">
     <button className="slideshow__button slideshow__button_previous" type="button" onClick={() => previousImg()} />
-    <img className="slideshow__image" src={authorImages[index].link}  />
+    <img className="slideshow__image" src={authorImages[index].link} ref={imgRef} />
     <button className="slideshow__button slideshow__button_next" type="button" onClick={() => nextImg()} />
   </div>);
 }
