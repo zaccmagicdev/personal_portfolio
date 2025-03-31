3d66537dddc4c82fa8759dff45de6c39e8606d95
Zachary Wedel <zwede710@gmail.com>
2025-03-31 08:14:18 -0500

    UPDATE: Added floatAround animation to slideshow

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -4,21 +4,18 @@ import { authorImages } from "../../constants/SlideShowImages";
 import generateRandom from "../../utils/utils";
 
 function SlideShow() {
-  const imgRef = React.useRef('');
   const [index, setIndex] = React.useState(0);
-  const arraySize = authorImages.length - 1;
+  const arraySize = authorImages.length - 2;
 
-  const addStatic = () => {}
-
-  const nextImg = () => {index >= arraySize ? setIndex(0) : setIndex(index + 1)}
-  const previousImg = () => {index <= 0 ? setIndex(arraySize) : setIndex(index - 1)}
+  const nextImg = () => {setIndex(4); setTimeout(() => index >= arraySize ? setIndex(0) : setIndex(index + 1), 1000)}
+  const previousImg = () => {setIndex(4); setTimeout(() => index <= 0 ? setIndex(arraySize) : setIndex(index - 1), 1000)}
   
-  React.useEffect(() => {setIndex(generateRandom(arraySize)); console.log(imgRef)}, [])
+  React.useEffect(() => {setIndex(generateRandom(arraySize))}, [])
 
   return (
   <div className="slideshow">
     <button className="slideshow__button slideshow__button_previous" type="button" onClick={() => previousImg()} />
-    <img className="slideshow__image" src={authorImages[index].link} ref={imgRef} />
+    <img className="slideshow__image" src={authorImages[index].link} />
     <button className="slideshow__button slideshow__button_next" type="button" onClick={() => nextImg()} />
   </div>);
 }
